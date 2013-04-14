ccb3fe8689f2045fd5d4c1b4fee898c6fbad9563
Alessandro Artoni <artoale@gmail.com>
2013-04-14 15:20:25 +0200

    Add support for uid ranges query Performances bugfixes (logout from unused sessions)

--- rest.js
+++ rest.js
@@ -7,13 +7,29 @@ var config = require('./config.json');
 
 var mail = getMailer(config);
 
+function walkParts(tree, type, subtype) {
+    var retval;
+    if (Array.isArray(tree)) {
+        for (var i in tree) {
+            retval = walkParts(tree[i], type, subtype);
+            if (retval) {
+                return retval;
+            }
+        }
+    } else {
+        console.log(tree);
+        if (tree.type === type && tree.subtype === subtype) {
+            return tree;
+        }
+        return false;
+    }
+}
 app.configure(function () {
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(function (req, res, next) {
         var oneof = false;
-        console.log('asdasdasdad');
 
         if (req.headers.origin) {
             res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -30,7 +46,7 @@ app.configure(function () {
         if (oneof) {
             res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
         }
-        console.log(req.headers, res.headers);
+
         // intercept OPTIONS method
         if (oneof && req.method === 'OPTIONS') {
             res.send(200);
@@ -58,7 +74,6 @@ mail.connect()
                 };
                 out.push(boxOut);
             }
-            console.log(JSON.stringify(out));
             res.json(out);
 
 
@@ -70,12 +85,22 @@ mail.connect()
     });
 
     app.get('/boxes/:boxId', function (req, res) {
-        mail.status(req.params.boxId).then(function (box) {
-            res.json(box);
-        }).fail(function (err) {
-            res.json(err);
+        var mail = getMailer(config);
+        mail.connect().then(function () {
+            mail.open(req.params.boxId, true).then(function (box) {
+                res.json(box);
+            }).fail(function (err) {
+                res.json(err);
+            })
+                .
+            finally(function () {
+                mail.logout();
+                console.log('Box: ' + req.params.boxId + ' closed');
+            });
         });
+
     });
+
     app.get('/boxes/:boxId/unseen', function (req, res) {
         mail.open(req.params.boxId).then(function () {
             return mail.search(['UNSEEN']);
@@ -94,14 +119,18 @@ mail.connect()
         });
     });
 
-    app.get('/boxes/:boxId/all', function (req, res) {
+    app.get('/boxes/:boxId/from/:lastSeenUid', function (req, res) {
         var mail = getMailer(config);
         mail.connect().then(function () {
-            mail.open(req.params.boxId).then(function () {
-                return mail.search(['ALL']);
-            }).then(function (uids) {
-                return mail.fetch(uids, {}, {
-                    fields: ['date','message-id','to','from','subject'],
+            return mail.open(req.params.boxId, true).then(function () {
+                var searchString = (parseInt(req.params.lastSeenUid, 10) + 1) + ':*';
+                return mail.fetch(searchString, {
+                    struct: true,
+                }, {
+                    headers: {
+                        fields: true, //['date', 'message-id', 'to', 'from', 'subject'],
+                        parse: false
+                    },
                     body: false,
                 });
             }).then(function (emails) {
@@ -109,26 +138,152 @@ mail.connect()
             }).fail(function (err) {
                 res.json(err);
             });
+        }).
+        finally(function () {
+            mail.logout();
         });
     });
 
+    /**
+     * Fetch emails from start+1 to end (extreme included)
+     */
+    app.get('/boxes/:boxId/from/:start/to/:end', function (req, res) {
+        var mail = getMailer(config);
+        mail.connect().then(function () {
+            return mail.open(req.params.boxId, true).then(function () {
+                var searchString = (parseInt(req.params.start, 10) + 1) + ':' + (parseInt(req.params.end, 10));
+                return mail.fetch(searchString, {
+                    struct: true,
+                }, {
+                    headers: {
+                        fields: true, //['date', 'message-id', 'to', 'from', 'subject'],
+                        parse: false
+                    },
+                    body: false,
+                });
+            }).then(function (emails) {
+                res.json(emails);
+            }).fail(function (err) {
+                res.json(err);
+            });
+        }).
+        finally(function () {
+            mail.logout();
+        });
+    });
+
+    // app.get('/boxes/:boxId/:mailId/:partId', function (req, res) {
+    //     var mail = getMailer(config);
+    //     mail.connect().then(function () {
+    //         return mail.open(req.params.boxId, true).then(function () {
+    //             return mail.fetch(req.params.boxId, {}, {
+    //                 id:req.params.partId,
+    //                 headers: false,
+    //                 body: true,
+    //             }, true);
+    //         }).then(function (emails) {
+    //             res.json(emails);
+    //         }).fail(function (err) {
+    //             res.json(err);
+    //         });
+    //     }).
+    //     finally(function () {
+    //         mail.logout();
+    //     });
+    // });
+    // app.get('/boxes/:boxId/all', function (req, res) {
+    //     // res.json(require('./eurecom.json'));
+    //     // return;
+    //     var mail = getMailer(config);
+    //     mail.connect().then(function () {
+    //         mail.open(req.params.boxId, true).then(function () {
+    //             var uids = [];
+    //             var limit = 30;
+    //             var today = new Date();
+    //             var daysToFetch = 10;
+    //             var start = new Date(today.getTime() - (1000 * 3600 * 24 * daysToFetch));
+    //             var end;
+
+    //             return mail.search(['ALL', ['SINCE', start]]).then(function reachLimit(fetchedUids) {
+    //                 uids = fetchedUids.concat(uids);
+    //                 if (uids.length >= limit) {
+    //                     uids.splice(29, uids.length - limit);
+    //                     return uids;
+    //                 } else {
+    //                     end = new Date(start.getTime());
+    //                     daysToFetch *= 2;
+    //                     start = new Date(end.getTime() - (1000 * 3600 * 24 * daysToFetch));
+    //                     return mail.search(['ALL', ['SINCE', start], ['BEFORE', end]]).then(reachLimit);
+    //                 }
+    //             });
+    //         }).then(function (uids) {
+    //             return mail.fetch(uids, {}, {
+    //                 fields: ['date', 'message-id', 'to', 'from', 'subject'],
+    //                 body: false,
+    //             });
+    //         }).then(function (emails) {
+    //             emails.sort(function emailCompare(a, b) {
+    //                 var aD = new Date(a.date);
+    //                 var bD = new Date(b.date);
+    //                 return -(aD.getTime() - bD.getTime());
+    //             });
+    //             res.json(emails);
+    //         }).fail(function (err) {
+    //             res.json(err);
+    //         }).
+    //         finally(function () {
+    //             mail.closeBox();
+    //         });
+    //     });
+    // });
+
     app.get('/boxes/:boxId/:mailId', function (req, res) {
-        mail.open(req.params.boxId).then(function () {
-            return mail.fetch(req.params.mailId, {}, {});
-        }).then(function (email) {
+        mail.open(req.params.boxId, true).then(function () {
+            console.log('Fetching email: ', req.params.mailId);
+            return mail.fetch(req.params.mailId, {}, {
+                // headers: {
+                //     parse: false
+                // },
+                // body: true,
+            });
+        })
+        // .then(function (email) {
+        //     console.log(email);
+        //     var part = walkParts(email[0].structure, 'text', 'html');
+        //     if (!part) {
+        //         part = walkParts(email[0].structure, 'text', 'plain');
+        //     }
+        //     return mail.fetch(req.params.mailId, {}, {
+        //         id: part,
+        //     },true).then(function (body) {
+        //         email[0].html = body;
+        //         return email;
+        //     });
+        // })
+        .then(function (email) {
+            console.log('Email: ', email[0].uid);
             res.json(email[0]);
         }).fail(function (err) {
+            console.error(err);
             res.json(err);
+        }).
+        finally(function () {
+            mail.closeBox().fail(function (err) {
+                console.error(err);
+            });
         });
     });
 
     app.get('/boxes/:boxId/:mailId/labels', function (req, res) {
-        mail.open(req.params.boxId).then(function () {
+        mail.open(req.params.boxId, true).then(function () {
             return mail.fetch(req.params.mailId, {}, {});
         }).then(function (email) {
             res.json(email[0]['x-gm-labels']);
         }).fail(function (err) {
             res.json(err);
+        }).
+        finally(function () {
+            mail.closeBox();
         });
     });
 
@@ -141,6 +296,10 @@ mail.connect()
             res.json(labels);
         }).fail(function (err) {
             res.json(err);
+        }).
+        finally(function () {
+            mail.open(req.params.boxId, true).then(mail.closeBox());
+
         });
     });
     app.listen(3000);
--- server.js
+++ server.js
@@ -7,33 +7,40 @@ var mail = require('./lib/mail')({
 mail.connect()
     .then(function () {
     console.log('Connected to gmail');
-    mail.boxes().then(function (boxes) {console.log(boxes);});
-    return mail.open('INBOX');
-})
-.then(function (box) {
-    console.log('Inbox opened: ', box.name);
-    return mail.search(['ALL', ['SINCE', new Date(2013, 3, 1, 20)],['X-GM-LABELS','eurecom']]);
-})
-.then(function (uids) {
-    var total = uids.length,
-        partial = 0;
-
-    console.log('Start Fetching:', uids);
-    return mail.fetch(uids, {}, {
-        body: false,
-    }).progress(function (mail) {
-        partial += 1;
-        console.log('Recived ' + partial + ' of ' + total + ' emails (', mail.from[0],')');
-
+    mail.boxes().then(function (boxes) {
+        console.log(boxes);
     });
+    return mail.open('Eurecom');
 })
-.then(function (mails) {
-    console.log('' + mails.length + ' emails recived');
+    .then(function (box) {
+    console.log('Inbox opened: ', box);
+    return mail.search(['ALL', ['SINCE', new Date(2013, 3, 1, 20)], ['X-GM-LABELS', 'eurecom']]);
 })
-.fail(function (err) {
+    .then(function (uids) {
+
+
+    // console.log('Start Fetching:', uids);
+    return mail.fetch(413, {
+        struct: true,
+    }, {
+        // id: '1.1.2',
+    },true);
+    // .progress(function (mail) {
+    //     partial += 1;
+    //     // console.log('Recived ' + partial + ' of ' + total + ' emails (', mail.from[0],')');
+
+    // });
+})
+    .then(function (mails) {
+    // console.log('' + mails.length + ' emails recived');
+        console.log(mails);
+    // console.log(JSON.stringify(mails[mails.length - 1]));
+})
+    .fail(function (err) {
     console.error(err);
 })
-.finally(function (what) {
+    .
+finally(function (what) {
     if (what) {
         console.log('exiting', what);
     }
