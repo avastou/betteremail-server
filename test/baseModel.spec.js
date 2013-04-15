6f374f50b9845aeebc20e5ea5c4799481c9cdb3a
Alessandro Artoni <artoale@gmail.com>
2013-04-15 19:18:58 +0200

    Add tests for baseModel Fix empty getById(promise is rejected instead of resolved with null)

--- test/baseModel.spec.js
+++ test/baseModel.spec.js
@@ -1,33 +1,36 @@
 'use strict';
 var baseModel,
-    di = require('di'),
+di = require('di'),
     db,
     config = {
         dbName: 'bettermailTest',
         dbHost: '127.0.0.1',
         dbPort: 27017
     },
-    injector;
+    injector,
+    Q = require('Q');
 
 
 var m = new di.Module();
 m.factory('db', require('../lib/models/db'));
 m.factory('baseModel', require('../lib/models/baseModel'));
 m.value('config', config);
-injector = new di.Injector([m]);
+var fix;
 var baseModel;
 describe('baseModel', function () {
 
-    beforeEach(function () {
+    beforeEach(function (done) {
 
         //Dependency injection test config
-
+        injector = new di.Injector([m]);
         db = injector.get('db');
+
         //reset the 'test' db with fixtures from json
         var fixtures = require('mongodb-fixtures');
-        fixtures.load(__dirname + '/fixtures');
-        fixtures.save(db, function () {
+        fix = fixtures.load(__dirname + '/fixtures');
+        fixtures.save(fix, db, function () {
             db.close();
+            done();
         });
 
         baseModel = injector.get('baseModel');
@@ -35,5 +38,43 @@ describe('baseModel', function () {
 
     it('should be defined', function () {
         expect(baseModel).toBeDefined();
+    });
+
+    it('should expose a _getById method', function () {
+        expect(baseModel._getById).toBeDefined();
+        expect(typeof baseModel._getById).toBe('function');
+    });
+
+    describe('_getById', function () {
+
+
+        it('should return a Q promise', function () {
+            var promise = baseModel._getById('516c2c85a25b4edb21000006', 'labels');
+            expect(Q.isPromise(promise)).toBe(true);
+        });
+
+        it('should fail for non-existent keys', function (done) {
+            baseModel._getById('516c2c85a25b4edb21000006', 'labels').fail(
+
+            function (reason) {
+                expect(reason).toBeDefined();
+                expect(reason.msg).toBeDefined();
+                expect(reason.msg).toBe('NO_RESULT');
+                done();
+
+            });
+        });
+
+        it('should find the right element', function (done) {
+            var testLabel1 = fix.labels[0];
+            // console.log(testLabel1._id.toString());
+            baseModel._getById(testLabel1._id.toString(), 'labels').then(function (label) {
+                expect(label).toBeDefined();
+                expect(label.name).toBe('test_label_1');
+                done();
+            });
+        });
+
+
     });
 });
